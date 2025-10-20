954cded1d10cfc2d3a81617020f8d410c7314851
Kevi5002 <93739889+Kevi5002@users.noreply.github.com>
2025-10-20 00:04:27 -0500

    First three questions
    Did first three task

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
-
+let favoriteFoods = ["Pizza", "Sushi", "Ramen", "Tacos", "Ice Cream", "Burgers"];
 
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
-
+for (let i = 0; i < favoriteFoods.length; i++) {
+    console.log("One of my favorite foods is " + favoriteFoods[i] + ".");
+}
 
 
 
@@ -14,7 +16,9 @@ console.log("Hello World!");
 // "My #2 favorite food is Sushi"
 // ...etc.
 
-
+for (let i = 0; i < favoriteFoods.length; i++) {
+    console.log("My #" + (i + 1) + " favorite food is " + favoriteFoods[i] + ".");
+}
 
 
 
